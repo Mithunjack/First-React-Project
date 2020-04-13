279420dbae188c1b2d014b422a53e5edbc3020df
Mithun Das <mithund654@gmail.com>
2020-04-13 14:15:08 +0200

    local json practice

--- src/App.tsx
+++ src/App.tsx
@@ -7,40 +7,21 @@ import Footer from "./components/Footer";
 import Form from "./components/Form";
 import Card from "./components/Card";
 import Joke from "./components/Joke";
+import TestJson from "./components/TestJson";
 
 function App() {
+
+    const  data = TestJson();
+    const newData = data.map(joke => <Joke content = {{ question : joke.question, punchLine : joke.punchLine}} />);
+
   return (
     <div>
       {/*<NavBar />*/}
       {/*<MyInfo/>*/}
       {/*<Form/>*/}
-      <Joke
-          content = {{question : "How are you?", punchLine : "That doesnt even matter!!" }}
-      />
-      <Joke
-            content = {{punchLine : "not yet!But soon" }}
-      />
-      <Joke
-            content = {{question : "what about your aim!", punchLine : "That doesnt even matter!!now!" }}
-      />
-      <Joke
-            content = {{question : "study!!", punchLine : "whats that!!" }}
-      />
 
-      {/*<Card*/}
-      {/*    content = {{imgurl : "https://googleplus-covers.com/covers/nature_balloon_ride.jpg",iconClass1 : "fa fa-arrow-circle-right cardreadme"*/}
-      {/*      ,iconClass2 : "fa fa-envelope-open"}}*/}
-      {/*/>*/}
-      {/*<Card*/}
-      {/*    content = {{imgurl : "https://car-images.bauersecure.com/upload/29533/images/480x270/001bmax03.jpg",*/}
-      {/*      iconClass1 : "fa fa-arrow-circle-right cardreadme",*/}
-      {/*      iconClass2 : "fa fa-envelope-open"}}*/}
-      {/*/>*/}
-      {/*<Card*/}
-      {/*    content = {{imgurl : "https://www.gannett-cdn.com/media/2017/10/14/USATODAY/USATODAY/636436189656790566-ABQ-hot-air-balloons2017-Trevor-Hughes1482.JPG?width=2560",*/}
-      {/*      iconClass1 : "fa fa-arrow-circle-right cardreadme",*/}
-      {/*      iconClass2 : "fa fa-envelope-open"}}*/}
-      {/*/>*/}
+      {newData}
+
       {/*<Footer/>*/}
     </div>
   );
@@ -64,3 +45,35 @@ export default App;
 {/*    </a>*/}
 {/*  </header>*/}
 {/*</div>*/}
+
+/*Single joker call*/
+{/*<Joke*/}
+{/*    content = {{question : "How are you?", punchLine : "That doesnt even matter!!" }}*/}
+{/*/>*/}
+{/*<Joke*/}
+{/*      content = {{punchLine : "not yet!But soon" }}*/}
+{/*/>*/}
+{/*<Joke*/}
+{/*      content = {{question : "what about your aim!", punchLine : "That doesnt even matter!!now!" }}*/}
+{/*/>*/}
+{/*<Joke*/}
+{/*      content = {{question : "study!!", punchLine : "whats that!!" }}*/}
+{/*/>*/}
+
+
+/*
+single card call*/
+{/*<Card*/}
+{/*    content = {{imgurl : "https://googleplus-covers.com/covers/nature_balloon_ride.jpg",iconClass1 : "fa fa-arrow-circle-right cardreadme"*/}
+{/*      ,iconClass2 : "fa fa-envelope-open"}}*/}
+{/*/>*/}
+{/*<Card*/}
+{/*    content = {{imgurl : "https://car-images.bauersecure.com/upload/29533/images/480x270/001bmax03.jpg",*/}
+{/*      iconClass1 : "fa fa-arrow-circle-right cardreadme",*/}
+{/*      iconClass2 : "fa fa-envelope-open"}}*/}
+{/*/>*/}
+{/*<Card*/}
+{/*    content = {{imgurl : "https://www.gannett-cdn.com/media/2017/10/14/USATODAY/USATODAY/636436189656790566-ABQ-hot-air-balloons2017-Trevor-Hughes1482.JPG?width=2560",*/}
+{/*      iconClass1 : "fa fa-arrow-circle-right cardreadme",*/}
+{/*      iconClass2 : "fa fa-envelope-open"}}*/}
+{/*/>*/}
---
+++ src/components/TestJson.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function TestJson() {
+    const JokerData = [
+        {
+            id: 1,
+            question : "How are you?",
+            punchLine : "That doesnt even matter!!"
+        },
+        {
+            id: 2,
+            punchLine : "not yet!But soon"
+        },
+        {
+            id: 3,
+            question : "what about your aim!",
+            punchLine : "That doesnt even matter!!now!"
+        },
+        {
+            id: 4,
+            question : "study!!",
+            punchLine : "whats that!!"
+        }
+    ]
+
+    return(
+        JokerData
+    )
+}
+
+export default TestJson
